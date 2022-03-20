50e73cc15ffa7c46d1cdf4e5b3ee506ea6334eeb
Patricio Villalobos <patricio@glacier.fi>
2022-03-20 14:45:52 -0300

    GFLW window

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,34 @@
 const std = @import("std");
 
+const glfw = @import("glfw");
+const zgl = @import("zgl");
+
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    try glfw.init(.{});
+    defer glfw.terminate();
+
+    const hints: glfw.Window.Hints = .{
+        .context_version_major = 3,
+        .context_version_minor = 2,
+        .opengl_profile = .opengl_core_profile,
+        .opengl_forward_compat = true,
+        .resizable = false,
+    };
+
+    const window = try glfw.Window.create(
+        640,
+        480,
+        "Hello ERE!",
+        null,
+        null,
+        hints,
+    );
+
+    defer window.destroy();
+
+    while (!window.shouldClose()) {
+        try glfw.pollEvents();
+    }
 }
 
 test "basic test" {
